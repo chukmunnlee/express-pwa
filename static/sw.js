a746083e8ac77bc689ca525bc906d58b88fa031f
Chuk <chukmunnlee@gmail.com>
2023-02-15 14:48:01 +0800

    sw initialize stage

--- static/sw.js
+++ static/sw.js
@@ -1,24 +1,24 @@
 const cacheName = 'mycache'
 
 const assets = [
-	'/icon',
+	'/favicon.ico',
 	'/manifest.json',
 	'/placeholder.png',
 	'/polar-bear.png',
 	'/styles.css',
-	'/static/icon/android-icon-192x192-seochecker-manifest-6612.png',
-	'/static/icon/apple-icon-114x114-seochecker-manifest-6612.png',
-	'/static/icon/apple-icon-120x120-seochecker-manifest-6612.png',
-	'/static/icon/apple-icon-144x144-seochecker-manifest-6612.png',
-	'/static/icon/apple-icon-152x152-seochecker-manifest-6612.png',
-	'/static/icon/apple-icon-180x180-seochecker-manifest-6612.png',
-	'/static/icon/apple-icon-57x57-seochecker-manifest-6612.png',
-	'/static/icon/apple-icon-60x60-seochecker-manifest-6612.png',
-	'/static/icon/apple-icon-72x72-seochecker-manifest-6612.png',
-	'/static/icon/apple-icon-76x76-seochecker-manifest-6612.png',
-	'/static/icon/favicon-16x16-seochecker-manifest-6612.png',
-	'/static/icon/favicon-32x32-seochecker-manifest-6612.png',
-	'/static/icon/favicon-96x96-seochecker-manifest-6612.png'
+	'/icon/android-icon-192x192-seochecker-manifest-6612.png',
+	'/icon/apple-icon-114x114-seochecker-manifest-6612.png',
+	'/icon/apple-icon-120x120-seochecker-manifest-6612.png',
+	'/icon/apple-icon-144x144-seochecker-manifest-6612.png',
+	'/icon/apple-icon-152x152-seochecker-manifest-6612.png',
+	'/icon/apple-icon-180x180-seochecker-manifest-6612.png',
+	'/icon/apple-icon-57x57-seochecker-manifest-6612.png',
+	'/icon/apple-icon-60x60-seochecker-manifest-6612.png',
+	'/icon/apple-icon-72x72-seochecker-manifest-6612.png',
+	'/icon/apple-icon-76x76-seochecker-manifest-6612.png',
+	'/icon/favicon-16x16-seochecker-manifest-6612.png',
+	'/icon/favicon-32x32-seochecker-manifest-6612.png',
+	'/icon/favicon-96x96-seochecker-manifest-6612.png'
 ]
 
 // Install cache
@@ -27,6 +27,15 @@ self.addEventListener('install', async (event) => {
 		caches.open(cacheName)
 			.then (cache => {
 				return cache.addAll(assets)
+				/*
+				const proms = []
+				assets.forEach(v => 
+					proms.push(cache.add(v)
+						.catch(f => console.error(`File error: ${f}`))
+					)
+				)
+				return Promise.all(proms)
+				*/
 			}
 		)
 	)
